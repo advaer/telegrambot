b464bb7bba4160c9d142b13c30a8f7e20d79b976
Rinat Akhmedziiev <r.akhmedziiev@levi9.com>
2017-08-28 17:44:09 +0300

    Added indexes

--- chalicelib/db/models.py
+++ chalicelib/db/models.py
@@ -19,10 +19,10 @@ class Currency(Base):
     __tablename__ = 'currencies'
 
     id = Column(Integer, primary_key=True)
-    base = Column(String(3))
-    counter = Column(String(3))
+    base = Column(String(3), index=True)
+    counter = Column(String(3), index=True)
     last = Column(Float(precision=6))
-    created_at = Column(DateTime())
+    created_at = Column(DateTime(), index=True)
 
     def __repr__(self):
         return f"<Currency: {self.base}/{self.counter} - {self.rate}>"
@@ -32,9 +32,9 @@ class Ticker(Base):
     __tablename__ = 'tickers'
 
     id = Column(Integer, primary_key=True)
-    exchange = (Column(String(64)))
-    base = Column(String(5))
-    counter = Column(String(5))
+    exchange = Column(String(64), index=True)
+    base = Column(String(5), index=True)
+    counter = Column(String(5), index=True)
     last = Column(Float(precision=8))
     lowest_ask = Column(Float(precision=8))
     highest_bid = Column(Float(precision=8))
@@ -44,7 +44,7 @@ class Ticker(Base):
     is_frozen = Column(Boolean)
     highest_24h = Column(Float(precision=8))
     lowest_24h = Column(Float(precision=8))
-    created_at = Column(DateTime())
+    created_at = Column(DateTime(), index=True)
 
     def __repr__(self):
         return f"<CurrencyRate: {self.base}/{self.counter} - {self.last}>"
